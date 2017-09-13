27323517b7fffa5e8ce4b7bb0c1b528e20353235
Keleas <svyat01ad@gmail.com>
2017-09-13 22:53:08 +0300

    Исправил отступы в 1_2 v1.1

--- task-1_2.cxx
+++ task-1_2.cxx
@@ -15,26 +15,26 @@
 
 int main()
 {
-	int n;
-	std::cin >> n;
+    int n;
+    std::cin >> n;
 
-	int curNum = n, probe =2;
-	//checking for simplicity
-	bool check = false;
+    int curNum = n, probe =2;
+    //checking for simplicity
+    bool check = false;
 
-	while( curNum != 1 && probe*probe <= n )
-	{
-		if( curNum % probe != 0 ) probe++;
-		else
-		{
-			check = true;
-			curNum /= probe;
-			std::cout << probe << " ";
-		}
-	}
-	if( !check ) std::cout << n;
+    while( curNum != 1 && probe*probe <= n )
+    {
+        if( curNum % probe != 0 ) probe++;
+        else
+        {
+            check = true;
+            curNum /= probe;
+            std::cout << probe << " ";
+        }
+    }
+    if( !check ) std::cout << n;
 
-	return 0;
+    return 0;
 }
 
 
