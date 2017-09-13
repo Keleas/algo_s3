6cee42a2782c48dbe5081a645a8dc7cf06ceb780
Keleas <svyat01ad@gmail.com>
2017-09-13 20:36:56 +0300

    Убрал недочеты в 1_6

--- task-1_6.cxx
+++ task-1_6.cxx
@@ -15,20 +15,22 @@ n ≤ 10000.
 
 int main()
 {
-    int n;
-    std::cin >> n;
-    int *arr = new int[n];
+  int n;
+  std::cin >> n;
+  int *arr = new int[n];
 
-    for ( int i = 0; i < n ; ++i )
-        std::cin >> arr[i];
+  for ( int i = 0; i < n ; ++i )
+    std::cin >> arr[i];
 
-    for ( int i = 0; i < n/2; ++i )
-        std::swap(arr[i] , arr[n - i -1] );
+  for ( int i = 0; i < n/2; ++i )
+    std::swap(arr[i], arr[n - i - 1] );
 
-    for ( int i = 0; i < n ; ++i )
-        std::cout << arr[i] << " ";
+  for ( int i = 0; i < n ; ++i )
+    std::cout << arr[i] << " ";
+  
+  delete [] arr;
 
-    return 0;
+  return 0;
 }
 
 
