7cde812f8bb39ca8c9896990ce8809b703d57946
Keleas <svyat01ad@gmail.com>
2017-09-13 20:36:42 +0300

    Убрал недочеты в 1_4

--- task-1_4.cxx
+++ task-1_4.cxx
@@ -12,34 +12,29 @@ n ≤ 10^7.
 ****************************************************************************/
 
 #include <iostream>
-#include <cmath>
-
 
 int fsd ( int n ) // first simple divisor
 {
-    for ( int i = 2; i <= sqrt(n); ++i )
-        if ( n % i == 0 ) return i;
-	
-    return 1;
+  for ( int i = 2; i*i <= n; ++i )
+    if ( n % i == 0 ) return i;
+
+  return 1;
 }
 
 int main()
 {
-    int n;
-    std::cin >> n;
+  int n;
+  std::cin >> n;
 
-    if ( fsd(n) != 1 )
-    {
-        int div = fsd (n);
-        if ( div != 1 ) 
-			std::cout 	<< n / div << " " 
-						<< (div - 1) * (n / div) 
-						<< std :: endl;
-     }
-    else std::cout 	<< '1' << " " << n 
-					<< std::endl;
+  if ( fsd(n) != 1 )
+  {
+    int div = fsd (n);
+    if ( div != 1 ) 
+      std::cout << n / div << " " << (div - 1) * (n / div) << std::endl;
+   }
+  else std::cout << '1' << " " << n-1 << std::endl;
 
-    return 0;
+  return 0;
 }
 
 
