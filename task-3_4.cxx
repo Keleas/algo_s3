f59c97155b4434657da9c48c166c96d0a964e53e
Keleas <svyat01ad@gmail.com>
2017-09-17 01:50:46 +0300

    Добавил решение 3_4

--- task-3_4.cxx
+++ task-3_4.cxx
@@ -18,16 +18,60 @@ n ≤ 110000, m ≤ 1000.
  in          | out
 -------------|-----------
  3           | 0 0 0 2
- 10 20 30    |        
- 4           |        
- 8 9 10 32   |        
+ 10 20 30    |
+ 4           |
+ 8 9 10 32   |
 
 ****************************************************************************/
 
 #include <iostream>
 
+/****************************************************************************
+Идея:
+бинарным поиском ищем 2 элемента массива A[],
+между которыми лежит наше число из B[],
+а затем смотрим, где разница меньше:
+от левого числа или правого, и соответственно выводим что подходит.
+****************************************************************************/
+int binSearch(const int key, const int *arr, const int size)
+{
+    int left = -1;
+    int right = size;
+    while(left < right -1)
+    {
+        int mid = (left+right) / 2;
+        if(arr[mid] <= key)
+            left = mid;
+        else if (arr[mid] >= key)
+            right = mid;
+    }
+    //Небольшой костыль для правильного вывода крайних случаев:
+    //нужный элемент самый первый или последний.
+    if(right == size) --right;
+    if(left == -1) ++left;
+    return (key-arr[left] <= arr[right]-key) ? left : right;
+}
+
 int main()
 {
+    int n;
+    std::cin >> n;
+    int* A = new int[n];
+    for(int i = 0; i < n; ++i)
+        std::cin >> A[i];
+
+    int m;
+    std::cin >> m;
+    int* B = new int[m];
+    for(int i = 0; i < m; ++i)
+        std::cin >> B[i];
+
+    for(int i = 0; i < m; ++i)
+        std::cout << binSearch(B[i], A, n) << " ";
+
+    delete [] A;
+    delete [] B;
+
     return 0;
 }
 
