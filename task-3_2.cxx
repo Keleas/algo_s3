4a278c8b2bb45cedcce2bc60af29633bc348df1f
Keleas <svyat01ad@gmail.com>
2017-09-16 23:25:21 +0300

    Добавил решение 3_2

--- task-3_2.cxx
+++ task-3_2.cxx
@@ -9,14 +9,37 @@
   in                   | out
   ---------------------|-----
   10                   | 6
-  1 2 3 4 5 6 7 6 5 4  | 
+  1 2 3 4 5 6 7 6 5 4  |
 
 ****************************************************************************/
 
+
 #include <iostream>
 
+//similar to binary search
+//in this problem we take a partition into 3 segments
 int main()
 {
+    int n;
+    std::cin >> n;
+    int* A = new int[n];
+    for(int i = 0; i < n; ++i)
+        std::cin >> A[i];
+
+    int left = -1;
+    int right = n;
+    while(right - left >= 3)
+    {
+        int mid1 = left + (right-left)/3;
+        int mid2 = right - (right-left)/3;
+
+        if(A[mid1] < A[mid2]) left = mid1;
+        else right = mid2;
+    }
+    std::cout << right -1 << std::endl;
+
+    delete [] A;
+
     return 0;
 }
 
